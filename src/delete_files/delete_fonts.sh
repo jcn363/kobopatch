6ae8698d5a45e703894a6f7dd61a80dd9594cec1
jcn363 <32136497+jcn363@users.noreply.github.com>
2018-12-07 08:45:41 +0100

    Add files via upload

---
+++ src/delete_files/delete_fonts.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/Amasis*
+# rm /usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/Avenir*
+rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/Caecilia*
+# rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/georgia*
+rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/GillSans*
+rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/KBJ*
+rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/KoboNickel*
+rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/Malabar*
+rm usr/local/Trolltech/QtEmbedded-4.6.2-arm/lib/fonts/OpenDyslexic*
+
+rm /etc/udev/rules.d/delete_fonts.rules
+rm /root/delete_fonts.sh
